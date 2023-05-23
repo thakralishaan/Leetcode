dbe235e09cd2508237d47f3c91df14fa3baac6c4
Ishaan Thakral <73595460+thakralishaan@users.noreply.github.com>
2023-05-23 17:03:41 +0530

    Time: 146 ms (55.33%), Space: 69.4 MB (58.73%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -10,4 +10,19 @@ public:
         }
         return false;
     }
-};
+};  
+
+// class Solution{
+// public:
+//     bool containsDuplicate(vector<int>&nums){
+        
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+            
+//             if(nums[i]==nums[i+1]){
+//                 return  true;
+//             }
+//         }
+//         return false;
+//         }
+// };
