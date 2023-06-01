b46e8fe2ae71aeb9cb4b2b3d8b4f87cfe4582eb6
Ishaan Thakral <73595460+thakralishaan@users.noreply.github.com>
2023-06-02 02:00:22 +0530

    Time: 29 ms (90.41%), Space: 25.9 MB (76.01%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,15 +1,47 @@
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+// //         int n=nums.size();
+        
+// //         for(int i=0;i<n;i++){
+            
+// //             nums[i]=(nums[i]*nums[i]);
+            
+// //         }
+        
+// //         sort(nums.begin(),nums.end());
+// //         return nums;
+// //     }
+// // };
+        
+  
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
+        vector<int> result(n);
         
-        for(int i=0;i<n;i++){
+        int left = 0;  // pointer for the leftmost negative number
+        int right = n - 1;  // pointer for the rightmost positive number
+        int index = n - 1;  // index to fill the result vector
+        
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
             
-            nums[i]=(nums[i]*nums[i]);
+            if (leftSquare > rightSquare) {
+                result[index] = leftSquare;
+                left++;
+            } else {
+                result[index] = rightSquare;
+                right--;
+            }
             
+            index--;
         }
         
-        sort(nums.begin(),nums.end());
-        return nums;
+        return result;
     }
 };
+
