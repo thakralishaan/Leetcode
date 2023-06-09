50c6294bb9a4b8110611c31e05f95fd2554ff276
Ishaan Thakral <73595460+thakralishaan@users.noreply.github.com>
2023-06-09 13:06:24 +0530

    Time: 8 ms (56.16%), Space: 7 MB (34.76%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, char> m1, m2;
+        for (int i = 0; i < s.size(); i++) {
+            if (!m1.count(s[i]) && !m2.count(t[i])) {
+                m1[s[i]] = t[i];
+                m2[t[i]] = s[i];
+            } else {
+                if (m1[s[i]] != t[i] || m2[t[i]] != s[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
