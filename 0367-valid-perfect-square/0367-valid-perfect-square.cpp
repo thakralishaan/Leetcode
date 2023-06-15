55db1f09fa6b395a12a04519f920f19be9e48e2b
Ishaan Thakral <73595460+thakralishaan@users.noreply.github.com>
2023-06-15 17:06:56 +0530

    Time: 3 ms (43.26%), Space: 6.1 MB (9.13%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,13 +1,44 @@
-class Solution {
-public:
-    bool isPerfectSquare(long long int num) {
-        for(long long int i=0;i<=num;i++){
+// class Solution {
+// public:
+//     bool isPerfectSquare(long long int num) {
+//         for(long long int i=0;i<=num;i++){
             
-            if(i*i==num ){
+//             if(i*i==num ){
                 
-                return true;
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+//BRUTE FORCE APPROACH --> O(ROOT N)   
+//WHERE WE JUST CHECK IF EACH ITERATION SQUARE IS EQUAL TO NUM IF NOT THEN FALSE
+//ALWAYS CHANGE DATA TYPE TO LONG LONG WHEN WE HAVE RUNTIME ERROR
+
+//SECOND APPROACH  --> USE BIANRY SEARCH --> O(LOGN)
+class Solution{
+    public:
+        bool isPerfectSquare(long long int num){
+            
+            long long int left=1;
+            long long int right=num;
+            
+            while(left<=right){
+                
+                long long int mid = left+(right-left)/2;
+                
+                if(mid*mid>num){
+                    right=mid-1;
+                }
+                else if(mid*mid<num){
+                    left=mid+1;
+                }
+                else{
+                    return true;
+                }
             }
+            return false;
         }
-        return false;
-    }
 };
+
+
