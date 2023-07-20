e2de33cd6c52a9d772356ee71d5c0c0554f9978f
Ishaan Thakral <73595460+thakralishaan@users.noreply.github.com>
2023-07-20 22:16:24 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (38.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& arr) {
 //         //First -> use sort function but it is not in place solution with TC -> nlogn and Sc-. 0n
         
 // //         sort(nums.begin(),nums.end());
@@ -10,55 +10,49 @@ public:
         
         
 //         // Third -> 3 pointer approach best solution using of 3 pointers low mid and high
-        
-//         int low=0; 
-//         int mid=0;
-//         int high = nums.size()-1;
-//         while(mid<=high){      //important condition here we have 3 elements so we take mid<high and not low and high
-//         if(nums[mid]==0){
-//             swap(nums[low],nums[mid]);
-//             low++;
-//             mid++;
-//         }
-//         else if(nums[mid]==1){
-            
-//             mid++;
-//         }
-        
-//         else{
-//             // for 2
-//             swap(nums[mid], nums[high]);
-//             high--;
-//         }
-//     }
-        
-//     }
-void sortColors(vector<int>& nums) {
-    int n = nums.size();
-    int c0 = 0;
-    int c1 = 0;
-    int c2 = 0;
+    
+// void sortColors(vector<int>& nums) {
+//     int n = nums.size();
+//     int c0 = 0;
+//     int c1 = 0;
+//     int c2 = 0;
 
-    // Count the occurrences of 0s, 1s, and 2s
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == 0) {
-            c0++;
-        } else if (nums[i] == 1) {
-            c1++;
+//     // Count the occurrences of 0s, 1s, and 2s
+//     for (int i = 0; i < n; i++) {
+//         if (nums[i] == 0) {
+//             c0++;
+//         } else if (nums[i] == 1) {
+//             c1++;
+//         } else {
+//             c2++;
+//         }
+//     }
+
+//     // Assign the elements in the correct order
+//     for (int i = 0; i < c0; i++) {
+//         nums[i] = 0;
+//     }
+//     for (int i = c0; i < c0 + c1; i++) {
+//         nums[i] = 1;
+//     }
+//     for (int i = c0 + c1; i < n; i++) {
+//         nums[i] = 2;
+//     }
+// }
+    int n = arr.size();
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        } else if (arr[mid] == 1) {
+            mid++;
         } else {
-            c2++;
+            swap(arr[mid], arr[high]);
+            high--;
         }
-    }
-
-    // Assign the elements in the correct order
-    for (int i = 0; i < c0; i++) {
-        nums[i] = 0;
-    }
-    for (int i = c0; i < c0 + c1; i++) {
-        nums[i] = 1;
-    }
-    for (int i = c0 + c1; i < n; i++) {
-        nums[i] = 2;
     }
 }
 };
