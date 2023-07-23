6da0133e1cba986a38bbba167f5f70b28e93769d
Ishaan Thakral <73595460+thakralishaan@users.noreply.github.com>
2023-07-23 13:59:59 +0530

    Time: 17 ms (85.14%), Space: 19.5 MB (50.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-//         int count=0;
-//         int res=0;
+        int count=0;
+        int element=0;
         
-//         for(int i=0;i<nums.size();i++){
-//             if(count==0){
-//                 res=nums[i];
-//             }
-            
-//             if(nums[i]==res){ 
-//                 count+=1;
-//             }
-//             else{
-//                 count-=1;
-//             }
-//         }
-        
-//         return res;
-//         }
-        
-        map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second> nums.size()/2){
-                return it.first;
+            if(count==0){
+                element=nums[i];
+            }
+            
+            if(nums[i]==element){ 
+                count+=1;
+            }
+            else{
+                count-=1;
             }
         }
-        return -1;
-    }
+        
+        return element;
+        }
+        
+    //     map<int,int>mp;                          //seconf best using hashing but due to extra space 
+    //     for(int i=0;i<nums.size();i++){
+    //         mp[nums[i]]++;
+    //     }
+    //     for(auto it:mp){
+    //         if(it.second> nums.size()/2){
+    //             return it.first;
+    //         }
+    //     }
+    //     return -1;
+    // }
 };
